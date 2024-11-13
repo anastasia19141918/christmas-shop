8d3e424f9757e6dc02fc4686eefbff071bbae5e4
Anastasiya <anastasiya.anast.ru@mail.ru>
2024-11-13 10:47:25 +0300

    feat: add form js

--- src/js/creat.js
+++ src/js/creat.js
@@ -1,12 +1,14 @@
 export function creat(el, index) {
-  const div = document.createElement('div');
+    const div = document.createElement('div');
     div.classList.add('best-gifts_itemn');
-
+    
     const img = document.createElement('img');
     img.classList.add('best-gifts_img');
-    img.alt = el.name;
+    img.alt = el.category;
     img.src = el.img;
+    img.dataset.id = el.id;
 
+    
     const divDesk = document.createElement('div');
     divDesk.classList.add('best-gifts_desk');
 
@@ -16,14 +18,17 @@ export function creat(el, index) {
 
     if (h4.innerText === 'For Work') {
       h4.classList.add('card_title_work');
+      img.dataset.myAttribute = 'For Work';
     };
 
     if (h4.innerText === 'For Health') {
       h4.classList.add('card_title_health');
+      img.dataset.myAttribute = 'For Health';
     };
 
     if (h4.innerText === 'For Harmony') {
       h4.classList.add('card_title_harmony');
+      img.dataset.myAttribute = 'For Harmony';
     };
 
     const h3 = document.createElement('h3');
@@ -37,4 +42,5 @@ export function creat(el, index) {
     div.appendChild(divDesk);
     divDesk.appendChild(h4);
     divDesk.appendChild(h3);
-}
+};
+
--- src/js/modal.js
+++
@@ -1,9 +0,0 @@
-export class Modal {
-  constructor (classes) {
-    this.classes = classes;
-    this.modal = '';
-    this.modalContant = '';
-    this.modalCloseBtn = '';
-    this.overlay = '';
-  }
-}

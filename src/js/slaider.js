f52da98fe698a475aa182f1169a77b0832e7b0e0
Anastasiya <anastasiya.anast.ru@mail.ru>
2024-11-15 11:09:12 +0300

    feat: add slaider js

---
+++ src/js/slaider.js
@@ -0,0 +1,47 @@
+const slaiderPref =  document.getElementById('slaider__pref');
+const slaiderNext =  document.getElementById('slaider__next');
+
+const slaiderGallery = document.getElementById('slaider__gallery');
+const slaids = document.getElementById('slaids');
+const cards = document.querySelectorAll('.slaider__item');
+const itemn = document.querySelector('.slaider__item');
+
+const cardsnewArraw = Array.from(cards);
+const slaiderCount = cardsnewArraw.length;
+
+
+let slaiderIndex = 0;
+
+slaiderNext.addEventListener('click', function(){
+  slaiderIndex = (slaiderIndex + 1) % slaiderCount;
+  slaider();
+  
+});
+
+slaiderPref.addEventListener('click', function(){
+  slaiderIndex = (slaiderIndex - 1 + slaiderCount) % slaiderCount;
+  slaider();
+})
+
+function slaider() {
+  if(window.innerWidth > '767') {
+    const widthSlaider = ((slaids.clientWidth - '400') / 4);
+    const slaiderOffset = -slaiderIndex * widthSlaider;
+    slaiderGallery.style.transform = `translateX(${slaiderOffset}px)`;
+  }
+  
+  if (window.innerWidth < '767') {
+    const widthSlaider = '430';
+    const slaiderOffset = -slaiderIndex * widthSlaider;
+    slaiderGallery.style.transform = `translateX(${slaiderOffset}px)`;
+  };
+  if (window.innerWidth < '380') {
+    const widthSlaider = '350';
+    const slaiderOffset = -slaiderIndex * widthSlaider;
+    slaiderGallery.style.transform = `translateX(${slaiderOffset}px)`;
+  };
+};
+
+
+
+
